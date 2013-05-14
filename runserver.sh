104cd1c8d56a4e5abc739b4b74ba573a016c3a4c
Jakub Žitný <zitnyjak@fit.cvut.cz>
2013-05-14 08:51:47 +0200

    runserver in tmux env

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+# bash script to initialize testing environment
+# launches django dev webserver and custom multi-threaded redis listener
+# (c) Jakub Zitny, 2013
 
-python3 manage.py runserver 8081
+E="tmux send-keys Enter"
+
+tmux new-session -d -s tndw-back -n 'runserver'
+tmux send-keys "python3 manage.py runserver 0.0.0.0:8081"; $E
+tmux new-window -t tndw-back -n 'listener'
+tmux send-keys "python3 manage.py listener"; $E
+
+
